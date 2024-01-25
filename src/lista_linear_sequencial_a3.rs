2161f8cd29f3ac4bfc9fe7eb4950bf5e6a840149
Mello Birkan <mgbirkan@icloud.com>
2024-01-25 20:47:03 -0300

    Enhance Lista structure and update main.rs function call
    Expanded the functionality of the Lista structure in lista_linear_sequencial_a3.rs, adding new functions and changing variable naming conventions. Simultaneously, updated main.rs to call the main function from the newly added module lista_linear_sequencial_a3 instead of aula_2.

--- src/lista_linear_sequencial_a3.rs
+++ src/lista_linear_sequencial_a3.rs
@@ -2,15 +2,42 @@ const MAX: usize = 50;
 
 type TIPOCHAVE = i32;
 
+#[derive(Copy, Clone)]
 struct Registro {
     chave: TIPOCHAVE,
 }
 
 struct Lista {
     a: [Registro; MAX] ,
-    nroElem: u32
+    nro_elem: u32
+}
+
+impl Lista {
+    fn new() -> Self {
+        Self {
+            a: [Registro {chave: 0}; MAX],
+            nro_elem: 0
+        }
+    }
+
+    fn nroElem(&self) -> u32 {
+        self.nro_elem
+    }
+
+    fn print_lista(&self) {
+        print!("Lista [");
+        for chave in self.a {
+            if chave.chave == 0 {
+                continue
+            } else {
+                print!("{} ", chave.chave);
+            }
+        }
+        print!("]");
+    }
 }
 
 pub fn main() {
-
+    let mut lista: Lista = Lista::new();
+    lista.print_lista();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod aula_2;
 mod lista_linear_sequencial_a3;
 
+
 fn main() {
-    aula_2::main();
+    // aula_2::main();
+    lista_linear_sequencial_a3::main();
 }
