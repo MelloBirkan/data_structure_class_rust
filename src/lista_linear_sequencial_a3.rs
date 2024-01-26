34e527719cbd64354f68f8c15aff942e8401c41f
Mello Birkan <mgbirkan@icloud.com>
2024-01-25 22:07:13 -0300

    Refactor 'busca' method and add 'del' method in lista_linear_sequencial_a3
    Modified the 'busca' method to return an Option rather than an isize for better readability and error management. Added a 'del' method to delete a specified value from the list. Updated 'main' function accordingly to make use of these changes.

--- src/lista_linear_sequencial_a3.rs
+++ src/lista_linear_sequencial_a3.rs
@@ -36,13 +36,13 @@ impl Lista {
         println!("]");
     }
 
-    fn busca(&self, chave: TIPOCHAVE) -> isize {
+    fn busca(&self, chave: TIPOCHAVE) -> Option<usize> {
         for (i, valor) in self.a.iter().enumerate() {
             if valor.chave == chave {
-                return i as isize;
+                return Some(i);
             }
         }
-        -1
+        None
     }
 
     fn add(&mut self, reg: Registro, posicao: usize) -> bool {
@@ -58,13 +58,27 @@ impl Lista {
         self.nro_elem += 1;
         true
     }
+
+    fn del(&mut self, valor: TIPOCHAVE) -> bool {
+        let valor_i = match self.busca(valor) {
+            Some(valor) => valor,
+            None => return false,
+        };
+        let mut j: usize = self.nro_elem as usize;
+        while j > valor_i {
+            self.a[j - 1] = self.a[j];
+            j -= 1;
+        }
+        self.nro_elem -= 1;
+        true
+    }
 }
 
 pub fn main() {
     let mut lista: Lista = Lista::new();
-    println!("{}", lista.add(Registro { chave: 12 }, 2));
+    println!("{}", lista.add(Registro { chave: 12 }, 0));
 
-    println!("{}", lista.busca(12));
+    println!("{:?}", lista.del(12));
     println!("{}", lista.nro_elem());
     lista.print_lista();
 }
